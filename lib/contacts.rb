e7aa08a7a760f03caecfdf59295cc4698fbdd208
Jason J Cruz <jaycru06@yahoo.com>
2019-11-26 03:39:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |keys, values|
+    values.each do |key, value|
+      if key == favorite_ice_cream_flavors && value == "strawberry"
+        
 end
